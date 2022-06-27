a1c7d9318cc4179abfb91b6890ee41f66edf50fe
knicolari <88011702+knicolari@users.noreply.github.com>
2022-06-27 20:27:52 -0300

    Update red.tf

--- red.tf
+++ red.tf
@@ -36,10 +36,27 @@ resource "aws_security_group" "eks_sg" {
 
 
   ingress {
-    description      = "ssh "
-    from_port        = 22
-    to_port          = 22
+    description      = "hhtp"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  
+   ingress {
+    description      = "hhtps"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
 
   }
